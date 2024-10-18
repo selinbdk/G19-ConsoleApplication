7e70caaaeacb8b894e34868b85aecf92917c6dde
Selin <sbudak@cyh.com.tr>
2024-10-18 21:24:01 +0300

    feat: added displayIntro and displayMenu functions

--- src/ConsoleApplication.java
+++ src/ConsoleApplication.java
@@ -1,7 +1,79 @@
 public class ConsoleApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        displayIntro();
+		displayMenu();
+
     }
+
+    public static void displayIntro() {
+		
+		String[] teamMembers= {
+				
+		"*******************",
+		"*     GROUP19     *",
+		"*   SELIN BUDAK   *",
+		"* TUGBA CAGLAGONUL*",
+		"*   BEYZA AKIN    *",
+		"* BERKAY KARATAS  *",
+		"* FURKAN KAPUSUZ  *",
+		"*******************"
+				
+		};
+
+        
+        String[] smileyFace= {
+           
+           "|||||||||||",
+           "|  _   _  |",
+           "|  o   o  |",
+           "| \\_____/ |",
+           "\\_________/",
+   
+
+
+   };
+
+        String[] welcomeMessage= {
+        "__      _____ _    ___ ___  __  __ ___",
+        "\\ \\    / / __| |  /  _/ _ \\|  \\/  | __|",
+        " \\ \\/\\/ /| _|| |_ | (_|(_) | |\\/| | _| ",
+        "  \\_/\\_/ |___|____\\___\\___/|_|  |_|___|",
+            
+        };
+
+        int maxLength= Math.max(teamMembers.length,smileyFace.length);
+		
+
+		for(int i=0;i<maxLength;i++) 
+		{
+			String firstPattern= (i<teamMembers.length) ? teamMembers[i] : "";
+			String secondPattern= (i<smileyFace.length) ? smileyFace[i] : "";
+            System.out.printf("%s   %s%n",firstPattern,secondPattern);
+
+        }
+      
+        for (String message : welcomeMessage) {
+            System.out.printf("%s%n", message);
+        }
+	
+	}
+	
+	public static void displayMenu() {
+		System.out.println("\nMENU");
+		
+		System.out.println(
+				  " [A] Statistical Information about an array,\n"
+				+ " [B] Matrix Operations,\n"
+				+ " [C] Text Encryption/Decryption,\n"
+				+ " [D] Tic-tac-toe HotSeat,\n"
+				+ " [E] Terminate.\n");
+		
+		
+		//System.out.println("Select the action you want to perform:");
+		
+	}
+	
+	
 
 
 
