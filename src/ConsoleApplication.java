fa52b1a02911ebfc9868cf5921d1af8696c580fe
Selin <sbudak@cyh.com.tr>
2024-10-19 21:15:45 +0300

    feat: added tic-tac-toe board and completed displayMenu

--- src/ConsoleApplication.java
+++ src/ConsoleApplication.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ConsoleApplication {
     public static void main(String[] args) {
         displayIntro();
@@ -22,7 +24,7 @@ public class ConsoleApplication {
 
         
         String[] smileyFace= {
-           
+           "            ",
            "|||||||||||",
            "|  _   _  |",
            "|  o   o  |",
@@ -59,7 +61,10 @@ public class ConsoleApplication {
 	}
 	
 	public static void displayMenu() {
+		boolean operationResult=true;
+		String operation="";
 		System.out.println("\nMENU");
+		Scanner input=new Scanner(System.in);
 		
 		System.out.println(
 				  " [A] Statistical Information about an array,\n"
@@ -68,10 +73,80 @@ public class ConsoleApplication {
 				+ " [D] Tic-tac-toe HotSeat,\n"
 				+ " [E] Terminate.\n");
 		
+
+		do {
+
+			if(!operationResult){
+				System.out.print("Please enter a valid operation character:");
+
+			}else{
+				System.out.print("Select the action you want to perform:");
+			}
+			operation =input.nextLine();
+			operationResult= ((operation.equals("A")) || (operation.equals("B")) || (operation.equals("C")) || (operation.equals("D")) ||(operation.equals("E")));
+			
+		} while (!operationResult);
+
+
+		switch (operation) {
+			case "A":
+				optionA();
+				break;
+			case "B":
+				optionB();
+				break;
+			case "C":
+				optionC();
+				break;
+
+			case "D":
+				optionD();
+				break;
+			case "E":
+				optionE();
+				break;
+
+			default:
+				//burası dolacak
+		}
+
 		
-		//System.out.println("Select the action you want to perform:");
 		
 	}
+
+	public static void optionA() {}
+
+	public static void optionB() {}
+
+	public static void optionC() {}
+
+	public static void optionD() {
+		char[][] board= {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+		printGameBoard(board);
+
+
+
+
+	}
+
+	public static void printGameBoard(char board[][]){
+
+		System.out.println("-------------"); 
+		for (int i = 0; i < 3; i++) //iterates rows
+   		 {
+			for (int j = 0; j < 3; j++) //iterates columns
+			{
+				System.out.printf("| %c ", board[i][j]);
+			}
+        	System.out.print("|\n");
+    	}
+
+    	System.out.println("-------------"); 
+
+	}
+
+	public static void optionE() {}
+
 	
 	
 
